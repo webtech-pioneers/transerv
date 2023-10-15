6dd14ce8ae3a1cce0b03fbe814b2ef38e31cace8
Valiant Mi-ing <139857193+valiant31@users.noreply.github.com>
2023-10-15 16:25:34 +0800

    Write.js

--- main.js
+++ main.js
@@ -11,3 +11,41 @@ fetch(new Request('./list.json'))
         output.appendChild(paragraph);
       }
   });
+
+//   document.addEventListener('DOMContentLoaded', function () {
+//     const reservationForm = document.getElementById('reservationForm');
+//     const output = document.getElementById('output');
+
+//     reservationForm.addEventListener('submit', function (e) {
+//         e.preventDefault();
+
+//         const formData = new FormData(reservationForm);
+//         const formDataObject = {};
+//         formData.forEach((value, key) => {
+//             formDataObject[key] = value;
+//         });
+
+//         const fs = require('fs'); 
+
+//         fs.readFile('list2.json', 'utf8', (err, data) => {
+//             if (err) {
+//                 console.error(err);
+//                 return;
+//             }
+
+//             let jsonData = JSON.parse(data);
+
+//             // Append the new reservation data to the existing JSON array
+//             jsonData.push(formDataObject);
+
+//             // Write the updated JSON back to the file
+//             fs.writeFile('list2.json', JSON.stringify(jsonData), 'utf8', (err) => {
+//                 if (err) {
+//                     console.error(err);
+//                 } else {
+//                     output.innerText = 'Reservation saved successfully!';
+//                 }
+//             });
+//         });
+//     });
+// });
