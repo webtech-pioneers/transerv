4f4f69064f529a278210e9a0889cfc3b973fc54d
Valiant Mi-ing <139857193+valiant31@users.noreply.github.com>
2023-10-15 17:07:03 +0800

    DisplayRoomNames.js

--- javascript/home-script.js
+++ javascript/home-script.js
@@ -26,3 +26,48 @@ for (i = 0; i < col.length; i++) {
     }
   });
 }
+document.addEventListener('DOMContentLoaded', function() {
+  let currentPage = 1;
+  const roomsPerPage = 10;
+
+  const nextButton = document.getElementById('nextButton');
+  const prevButton = document.getElementById('prevButton');
+
+  fetch('rooms.json')
+      .then(response => response.json())
+      .then(data => {
+          function displayRooms() {
+              const startIndex = (currentPage - 1) * roomsPerPage;
+              const endIndex = startIndex + roomsPerPage;
+              const currentRooms = data.slice(startIndex, endIndex);
+
+              const boxes = document.querySelectorAll('.box');
+
+              boxes.forEach((box, index) => {
+                  if (index < currentRooms.length) {
+                      const room = currentRooms[index];
+                      const h3 = box.querySelector('h3');
+                      const p = box.querySelector('p');
+
+                      h3.textContent = room.roomName;
+                      p.textContent = `Price: $${room.price}`;
+                      box.style.display = 'block'; 
+                  } else {
+                      box.style.display = 'none';
+                  }
+              });
+              nextButton.disabled = currentPage * roomsPerPage >= data.length;
+              prevButton.disabled = currentPage <= 1;
+          }
+          displayRooms();
+          nextButton.addEventListener('click', function() {
+              currentPage++;
+              displayRooms();
+          });
+          prevButton.addEventListener('click', function() {
+              currentPage--;
+              displayRooms();
+          });
+      })
+      .catch(error => console.error(error));
+});
